e1e1f04dae5d852df46b84a15e0008e7a4312749
Artyom Rudavin <arrudavin7@gmail.com>
2017-05-10 21:41:39 +0300

    ok lesson

--- animate/js/script.js
+++ animate/js/script.js
@@ -1,3 +1,41 @@
-// alert('Hello world!');
-// $('body').css('background-color', 'blue');
+// FPS - Frames Per Second
 
+
+// javascript animation
+function slideLeft(el, duration, slideLength) {
+  var leftPosition = parseInt(el.style.left) || 0;
+  var time = 0;
+  var fps = 50;
+
+  console.log(leftPosition);
+
+  var interval = setInterval(function() {
+
+    if (time > duration) {
+      clearInterval(interval);
+    }
+
+    leftPosition += slideLength/(duration/fps);
+    time += duration/fps;
+    el.style.left = leftPosition + 'px';
+
+  }, duration/fps);
+};
+
+var box = document.querySelector('.js-box');
+
+box.addEventListener('click', function() {
+  slideLeft(box, 1000, 200);
+});
+
+//jquery animation
+$('.jquery-box').on('click', function() {
+  $(this).animate({
+    left: '+=100'
+  }, 1000);
+});
+
+//css animation
+$('.css-box').on('click', function() {
+  $(this).addClass('css-box-animated');
+});
