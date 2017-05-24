a634d4b90b01a8d69beb0dd86d41c46af9bc2928
Artyom Rudavin <arrudavin7@gmail.com>
2017-05-24 21:15:20 +0300

    end hm 12

--- shab/js/script.js
+++ shab/js/script.js
@@ -1,9 +1,25 @@
 $(function() {
 
-	var template = $('#test').html();
+	var html = $('#test').html();
+  var articles = [
+  {
+    title: 'Article 1',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil necessitatibus, nisi consequuntur, quis, numquam et recusandae, quasi nulla aliquam animi magnam. Earum cumque quos veritatis quas assumenda, provident dolorum itaque!'
+  },
+  {
+    title: 'Article 2',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+  },
+  {
+    title: 'Article 3',
+    content: 'Tempore enim dolore velit doloremque saepe officia, mollitia natus ullam ratione debitis placeat ex laborum quia aliquid culpa illo temporibus dicta officiis.'
+  }
+  ];
 
-	console.log('template', template);
+  var content = tmpl(html, {
+    data: articles
+  });
 
-	$('body').append(template);
+  $('body').append(content);
 
 });
