44ca86fa03cb79de681976d2e655f7105f608751
Artyom Rudavin <arrudavin7@gmail.com>
2017-05-27 14:15:05 +0300

    2

--- tryCatch/js/script.js
+++ tryCatch/js/script.js
@@ -1,1 +1,11 @@
-console.log('Ola!');
+var str = '{name: John Doe}';
+
+
+try {
+
+	var obj = JSON.parse(str);
+	console.log('obj.name', obj.name);
+
+} catch (e) {
+	alert('Wrong data, try to refresh the page');
+}
