7bcbc6f33c26c11cbcc97288fc4c8af65b15ba32
Artyom Rudavin <arrudavin7@gmail.com>
2017-05-30 19:06:03 +0300

    worked

--- XMLHttpRequestAjax/js/script.js
+++ XMLHttpRequestAjax/js/script.js
@@ -1,1 +1,13 @@
-console.log('Hello');
+// http://xmlhttprequest.ru/
+// https://ru.wikipedia.org/wiki/AJAX
+// https://ru.wikipedia.org/wiki/Список_кодов_состояния_HTTP
+// https://ru.wikipedia.org/wiki/Правило_ограничения_домена
+
+$(function() {
+
+	
+
+
+
+	
+});
