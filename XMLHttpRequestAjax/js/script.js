6bd98ab13ac669e2909fe032e9c07a3f69429668
Artyom Rudavin <arrudavin7@gmail.com>
2017-06-01 17:54:56 +0300

    xml

--- XMLHttpRequestAjax/js/script.js
+++ XMLHttpRequestAjax/js/script.js
@@ -3,11 +3,25 @@
 // https://ru.wikipedia.org/wiki/Список_кодов_состояния_HTTP
 // https://ru.wikipedia.org/wiki/Правило_ограничения_домена
 
+function GoogleCallback() {
+	console.log('data', data);
+}
+
+
 $(function() {
 
-	
+	$.ajax({
+		url: // key ?????
+		'http://ajax.googleapis.com/ajax/services/search/web?v=1.0?key=AIzaSyDkdHMRKfrMYrk3iv7t4cd0gdlKlq6pO9M&rsz=large&q=' + 'test' + '&callback=GoogleCallback&context=?',
+		dataType: 'jsonp',
+		success: function() {
+
+		},
+		error: function() {
+
+		}
+	});
 
 
 
-	
 });
