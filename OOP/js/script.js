89a360e76c98a3e2e11daee77a81c0fa42cbba65
Artyom Rudavin <arrudavin7@gmail.com>
2017-06-02 15:42:18 +0300

    a

--- OOP/js/script.js
+++ OOP/js/script.js
@@ -1,27 +1,2 @@
-// http://xmlhttprequest.ru/
-// https://ru.wikipedia.org/wiki/AJAX
-// https://ru.wikipedia.org/wiki/Список_кодов_состояния_HTTP
-// https://ru.wikipedia.org/wiki/Правило_ограничения_домена
+// console.log('Hello');
 
-function GoogleCallback() {
-	console.log('data', data);
-}
-
-
-$(function() {
-
-	$.ajax({
-		url: // key ?????
-		'http://ajax.googleapis.com/ajax/services/search/web?v=1.0?key=AIzaSyDkdHMRKfrMYrk3iv7t4cd0gdlKlq6pO9M&rsz=large&q=' + 'test' + '&callback=GoogleCallback&context=?',
-		dataType: 'jsonp',
-		success: function() {
-
-		},
-		error: function() {
-
-		}
-	});
-
-
-
-});
