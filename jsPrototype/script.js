2f2a6f481013ed032830a4b7f7c85f45d491583f
Artyom Rudavin <arrudavin7@gmail.com>
2017-07-11 17:47:54 +0300

    add proto

---
+++ jsPrototype/script.js
@@ -0,0 +1,16 @@
+var Track = function() {
+		
+};
+
+var track01 = new Track({
+	name: 'track01',
+	url: 'track01.mp3'
+});
+
+var track02 = new Track({
+	name: 'track02',
+	url: 'track02.mp3'
+});
+
+console.log(track01);
+console.log(track02);
