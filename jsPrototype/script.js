9993ebe539511a7f2efb2f77ec6ebfa1486569b5
Artyom Rudavin <arrudavin7@gmail.com>
2017-07-11 18:18:54 +0300

    finish proto

--- jsPrototype/script.js
+++ jsPrototype/script.js
@@ -1,5 +1,26 @@
-var Track = function() {
-		
+var Track = function(params) {
+	this.name = params.name;
+	this.url = params.url;
+
+	// this.playTrack = function() {
+	// 	console.log('We started playing', this.name);
+	// }
+};
+
+Track.prototype.playTrack = function() {
+	console.log('We started playing', this.name);
+};
+
+var YoutubeTrack = function(params) {
+	Track.apply(this, arguments);
+	this.image = params.image;
+};
+
+YoutubeTrack.prototype = Object.create(Track.prototype);
+YoutubeTrack.prototype.constructor = YoutubeTrack;
+
+YoutubeTrack.prototype.playTrack = function() {
+	console.log('Hello youtube', this.name);
 };
 
 var track01 = new Track({
@@ -14,3 +35,24 @@ var track02 = new Track({
 
 console.log(track01);
 console.log(track02);
+
+track01.playTrack();
+track02.playTrack();
+
+var youtubeTrack01 = new YoutubeTrack({
+	name: 'youtubeTrack01',
+	url: 'youtubeTrack01.mp3',
+	image: 'track01.jpg'
+});
+
+var youtubeTrack02 = new YoutubeTrack({
+	name: 'youtubeTrack02',
+	url: 'youtubeTrack02.mp3',
+	image: 'track02.jpg'
+});
+
+console.log(youtubeTrack01);
+console.log(youtubeTrack02);
+
+youtubeTrack01.playTrack();
+youtubeTrack02.playTrack();
